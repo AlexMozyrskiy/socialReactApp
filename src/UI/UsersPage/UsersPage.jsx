01a652c1ff0f056dc10ec167d96b6eaa156ab6b5
Alexey Mozyrskiy <mozyrski@gmail.com>
2021-02-01 14:26:43 +0300

    users page pagination html css

--- src/UI/UsersPage/UsersPage.jsx
+++ src/UI/UsersPage/UsersPage.jsx
@@ -7,10 +7,31 @@ import ButtonPreloader from "../common/ButtonPreloader";
 const UsersPage = (props) => {
     return (
         <div className="grid-wrapper__content users">
-            {
-                props.isPreloaderActive
-                    ? <AppPreloader />
-                    : <div className="users__users">
+            { props.isPreloaderActive
+                ? <AppPreloader />
+                : <>
+                    <div className="users__pagination-wrapper">
+                        <div className="users__pagination">
+                            <span className="users__pagination-single-span">&larr;</span>
+
+                            <span className="users__pagination-single-span users__pagination-single-span_active">1</span>
+                            <span className="users__pagination-single-span">2</span>
+                            <span className="users__pagination-single-span">3</span>
+                            <span className="users__pagination-single-span">4</span>
+                            <span className="users__pagination-single-span">5</span>
+                            <span className="users__pagination-single-span">6</span>
+                            <span className="users__pagination-single-span">7</span>
+                            <span className="users__pagination-single-span">8</span>
+                            <span className="users__pagination-single-span">9</span>
+                            <span className="users__pagination-single-span">10</span>
+
+                            <span className="users__pagination-single-span">...</span>
+                            <span className="users__pagination-single-span">Last Page</span>
+
+                            <span className="users__pagination-single-span">&rarr;</span>
+                        </div>
+                    </div>
+                    <div className="users__users">
 
                         {
                             props.usersArray.map(singleUser => {
@@ -31,6 +52,7 @@ const UsersPage = (props) => {
                             <ButtonPreloader displayNone={!props.isButtonLoadMoreUsersClicked} dotsCount={10} classes={["loader", "button_show-more-users"]} />
                         </div>
                     </div>
+                </>
 
             }
 
