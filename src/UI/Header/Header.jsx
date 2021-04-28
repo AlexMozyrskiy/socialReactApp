5ccc9dc0f52de910e440ec92c2f7b14894261df1
Alexey Mozyrskiy <mzyrski@gmail.com>
2021-04-28 17:06:49 +0300

    predeploy

--- src/UI/Header/Header.jsx
+++ src/UI/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = (props) => {
                 <div className="header__flex-wrapper">
 
                     <div className="header__logo-and-title">
-                        <img src="#" alt="logo" className="header__logo" />
+                        {/* <img src="#" alt="logo" className="header__logo" /> */}
                         <h2 className="header__title">JS Friend Finder</h2>
                     </div>
 
